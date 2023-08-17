308130f1e9a70d2df31cfc2733caf58abd59483d
Anton Platonov <platosha@gmail.com>
2023-08-17 18:14:17 +0300

    proto: use packages from Hilla repo branch

--- frontend/to-be-generated/com/example/application/endpoints/helloreact/FormEndpoint/Entity.ts
+++
@@ -1,7 +0,0 @@
-interface Entity {
-    choice?: string;
-    date?: string;
-    name?: string;
-    number: number;
-}
-export default Entity;
--- frontend/views/helloreact/HelloReactView.tsx
+++ frontend/views/helloreact/HelloReactView.tsx
@@ -3,29 +3,27 @@ import { ComboBox } from '@hilla/react-components/ComboBox.js';
 import { DatePicker } from '@hilla/react-components/DatePicker.js';
 import { NumberField } from '@hilla/react-components/NumberField.js';
 import { TextField } from '@hilla/react-components/TextField.js';
-import { useField, useBinder } from 'Frontend/form/binder.js';
+import { FieldDirectiveResult, useBinder } from '@hilla/react-form';
 import { FormEndpoint } from 'Frontend/generated/endpoints';
-import EntityModel from 'Frontend/to-be-generated/com/example/application/endpoints/helloreact/FormEndpoint/EntityModel';
+import EntityModel from 'Frontend/generated/com/example/application/endpoints/helloreact/FormEndpoint/EntityModel';
 import { useState } from 'react';
+import { AbstractModel } from '@hilla/form';
 
 const comboBoxItems = ['foo', 'bar'];
 
 export default function FormView() {
   const [ name, setName ] = useState('');
 
-  const { model, submit, } = useBinder(EntityModel, {onSubmit: FormEndpoint.sendEntity});
-  const nameField = useField(model.name);
-  const choiceField = useField(model.choice);
-  const numberField = useField(model.number);
-  const dateField = useField(model.date);
+  const { binder: { model, submit }, field: originalField } = useBinder(EntityModel, { onSubmit: FormEndpoint.sendEntity });
+  const field = originalField as <M extends AbstractModel<any>>(model: M) => FieldDirectiveResult;
 
   return (
     <>
       <section className="flex p-m gap-m items-baseline">
-        <TextField label="Name" {...nameField}></TextField>
-        <ComboBox label="Choose" {...choiceField} items={comboBoxItems}></ComboBox>
-        <NumberField label="Number" {...numberField}></NumberField>
-        <DatePicker label="Date" {...dateField}></DatePicker>
+        <TextField label="Name" {...field(model.name)}></TextField>
+        <ComboBox label="Choose" {...field(model.choice)} items={comboBoxItems}></ComboBox>
+        <NumberField label="Number" {...field(model.number)}></NumberField>
+        <DatePicker label="Date" {...field(model.date)}></DatePicker>
         <Button onClick={submit}>submit</Button>
       </section>
     </>
