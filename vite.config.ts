41f512328aa78c1f70abc10fd043ee61d0d0c896
Anton Platonov <platosha@gmail.com>
2023-08-18 17:44:11 +0300

    proto: hack Vite config for instant Hilla TypeScript source updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import react from '@vitejs/plugin-react';
-import type { UserConfigFn } from 'vite';
+import type { PluginOption, UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
+import {readFile} from 'node:fs/promises'
+import {resolve} from 'node:path';
+
+const hillaFormVersion = JSON.parse(await readFile(resolve('./frontend/hilla/packages/ts/form/package.json'), { encoding: 'utf-8' })).version;
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
@@ -10,11 +14,14 @@ const customConfig: UserConfigFn = (env) => ({
       include: '**/*.tsx',
     }),
   ],
-  optimizeDeps: {
-    exclude: [
-      '@hilla/form',
-      '@hilla/react-form',
-    ],
+  define: {
+    '__VERSION__': `'${hillaFormVersion}'`,
+  },
+  resolve: {
+    alias: {
+      '@hilla/form': resolve('./frontend/hilla/packages/ts/form/src/index.ts'),
+      '@hilla/react-form': resolve('./frontend/hilla/packages/ts/react-form/src/index.ts'),
+    },
   },
 });
 
